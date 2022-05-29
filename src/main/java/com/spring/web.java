5dfe5a02455c84d5148f3f2441ae95047384a498
Gopi Vuthukota <gopiraj.raj87@gmail.com>
2022-05-29 15:39:05 +0530

    git first commit

--- src/main/java/com/spring/web.java
+++ src/main/java/com/spring/web.java
@@ -2,6 +2,7 @@ package com.spring;
 
 public class web {
 	public static void main(String args[]) {
-		System.out.println("Hi");
+		System.out.println("Hi Hello");
+		System.out.println("This is Gopi");
 	}
 }
